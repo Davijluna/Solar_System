74de0ed5ed3f0343278b686dcc8af3d4ccc15abc
Davi Luna <davijluna@gmail.com>
2022-04-06 17:58:19 -0300

    requesito 2

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Hearder() {
-  return (
-    <header>
-      <h1>Sistema Solar</h1>
-    </header>
-  );
-}
---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        a
+      </div>
+    );
+  }
+}
+export default SolarSystem;
